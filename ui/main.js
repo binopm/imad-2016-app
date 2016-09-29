8b4b111fc6cf3d17b0f12670bc7989d63444af4f
binopm <bino.opil@gmail.com>
2016-09-29 21:26:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 var element=document.getElementById('main-text'
 );
 element.InnerHtml = 'New Value';
+
+var madi=document.getElementById('madi');
+madi.onclick=function(){
+  madi.style.marginleft='100px' ;  
+};
