a255e0f8ffb125460023a3d8c79cc41b40848d96
binopm <bino.opil@gmail.com>
2016-09-29 21:45:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ var articles= {
   `<p>
     This is the content of my third article.
     </p>`
-};
+}
 };
 function CreateTemplate(data){
 var title=data.title;
@@ -81,6 +81,11 @@ return HtmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var counter = 0;
+app.get('/counter',function(req,res){
+   counter = counter + 1; 
+    res.send(counter.tostring());
 });
 
 app.get('/:articleName',function(req,res){
